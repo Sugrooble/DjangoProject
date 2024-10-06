ef2eb3239ae5f14e9a63c9a537b8aefb03fd80b4
drozdvasily <drozdvasilij21@gmail.com>
2024-10-06 13:38:36 +0300

    create_category_db

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -7,6 +7,14 @@ class PublishedManager(models.Manager):
         return super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Women(models.Model):
     class Status(models.IntegerChoices):
         DRAFT = (0, 'Черновик')
@@ -18,6 +26,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    cat = models.ForeignKey(Category, on_delete=models.PROTECT)
 
     published = PublishedManager()
     objects = models.Manager()
